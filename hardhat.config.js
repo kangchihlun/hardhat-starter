b4b7290efd7a745c864c88366319e3daf403f873
kang chih lun <kangchihlun@gmail.com>
2022-09-20 06:31:45 +0800

    add forking start block number

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,7 +15,8 @@ module.exports = {
     networks: {
       hardhat: {
         forking: {
-          url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`
+          url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+          //blockNumber: 15058412
         }
       },
       goerli: {
